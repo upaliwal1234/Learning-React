9edb9f81af4abf755e7d42a54e450815f4ba8627
Utkarsh Paliwal <utkarshpaliwal1234@gmail.com>
2023-10-07 22:30:12 +0530

    counter using hooks

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+  let [counter, setCounter] = useState(15)
+
+  const addValue = () =>{
+    setCounter(counter + 1);
+  }
+
+  const removeValue = () =>{
+    setCounter(counter - 1);
+  }
+
+  return (
+    <>
+      <h1>React</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add Value {counter}</button>
+      <br />
+      <br />
+      <button
+      onClick={removeValue}
+      >remove value {counter}</button>
+    </>
+  )
+}
+
+export default App
