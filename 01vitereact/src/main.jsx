f397d4334cb1c3a1a4451055b6d7b314cfd3de43
Utkarsh Paliwal <utkarshpaliwal1234@gmail.com>
2023-10-07 22:29:19 +0530

    code added

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,20 +2,24 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-function MyApp() {
-  return(
-    <div>
-      <h1>Custom App</h1>
-    </div>
-  )
-}
+// function MyApp() {
+//   return(
+//     <div>
+//       <h1>Custom App</h1>
+//     </div>
+//   )
+// }
+
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'htttps://google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
+    // ReactElement
     <App/>
-      {/* MyApp() */}
-    <MyApp/>
-  </>
-
-    
 )
